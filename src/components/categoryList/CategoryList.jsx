57328e24240a28f1a3a26ec6797696d83ddaab3d
Sulaimon Omotosho <susudef2u@gmail.com>
2024-05-22 23:12:09 +0100

    Feat: Added Featured post and popular categories to home page

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
 import styles from './categoryList.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const categories = [
+  { img: '/style.png', cat: 'style', color: '#57c4ff31' },
+  { img: '/fashion.png', cat: 'fashion', color: '#da85c731' },
+  { img: '/food.png', cat: 'food', color: '#7fb88133' },
+  { img: '/travel.png', cat: 'travel', color: '#ff795736' },
+  { img: '/culture.png', cat: 'culture', color: '#ffb04f45' },
+  { img: '/coding.png', cat: 'coding', color: '#5e4fff31' },
+]
 
 const CategoryList = () => {
-  return <div className={styles.container}>CategoryList</div>
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        {categories.map((cat, idx) => (
+          <Link
+            key={idx}
+            href={`/blog?cat=${cat.cat}`}
+            className={styles.category}
+            style={{ background: cat.color }}
+          >
+            <Image
+              src={cat.img}
+              alt={cat.cat}
+              width={32}
+              height={32}
+              className={styles.image}
+            />
+            {cat.cat}
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default CategoryList
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import styles from './featured.module.css'
+import Image from 'next/image'
 
 const Featured = () => {
-  return <div className={styles.container}>Featured</div>
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, Yall!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image
+            src='/p1.jpeg'
+            alt='placeholder'
+            fill
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet.</h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+            quaerat nam fugiat dignissimos iusto nihil accusamus maxime.
+            Obcaecati, corrupti asperiores.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Featured
