37e03f9f6ebfffecb1380141cfc97797702a2476
Sulaimon Omotosho <Susudef2u@gmail.com>
2024-05-28 19:14:17 +0100

    Update page.jsx
    Imported dynamics into Post.page

--- src/app/post/page.jsx
+++ src/app/post/page.jsx
@@ -14,6 +14,7 @@ import {
 } from 'firebase/storage'
 import { app } from '@/utils/firebase'
 import { categories } from '@/constants'
+import dynamic from 'next/dynamic'
 
 const storage = getStorage(app)
 
