7b655f2e96ddba00e58df46f28bf4e9bd55ebbbe
Sulaimon Omotosho <susudef2u@gmail.com>
2024-05-29 10:23:13 +0100

    Chore: Bug Fixing

--- src/app/api/popular/route.js
+++ src/app/api/popular/route.js
@@ -1,30 +1,46 @@
 import prisma from '@/utils/connect'
 import { NextResponse } from 'next/server'
 
-export const GET = async () => {
-  const POST_PER_PAGE = 6
+// export const GET = async () => {
+//   const POST_PER_PAGE = 6
 
-  const query = {
-    take: POST_PER_PAGE,
-    orderBy: {
-      views: 'desc',
-    },
-    include: { cat: true, user: true },
-  }
+//   const query = {
+//     take: POST_PER_PAGE,
+//     orderBy: {
+//       views: 'desc',
+//     },
+//     include: { cat: true, user: true },
+//   }
 
+//   try {
+//     const popular = await prisma.post.findMany(query)
+
+//     return new NextResponse(JSON.stringify(popular, { status: 200 }))
+//   } catch (err) {
+//     console.error(err)
+//     return new NextResponse(
+//       JSON.stringify(
+//         {
+//           message: 'something went wrong!',
+//         },
+//         { status: 500 }
+//       )
+//     )
+//   }
+// }
+
+export default async function handler(req, res) {
   try {
-    const popular = await prisma.post.findMany(query)
-
-    return new NextResponse(JSON.stringify(popular, { status: 200 }))
-  } catch (err) {
-    console.error(err)
-    return new NextResponse(
-      JSON.stringify(
-        {
-          message: 'something went wrong!',
-        },
-        { status: 500 }
-      )
-    )
+    const data = await prisma.post.findMany({
+      orderBy: {
+        views: 'desc',
+      },
+      include: { cat: true, user: true },
+      take: 6,
+    })
+    res.status(200).json({ posts: data })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ posts: [] })
   }
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,9 +14,9 @@ const Navbar = () => {
         <Image src='/tiktok.png' alt='tiktok' width={24} height={24} />
         <Image src='/youtube.png' alt='youtube' width={24} height={24} />
       </div> */}
-      <Link href='/' className={styles.logo}>
-        Uncle Sula
-      </Link>
+      <div className={styles.logo}>
+        <Link href='/'>Uncle Sula</Link>
+      </div>
       <div className={styles.right}>
         <ThemeToggle />
         <div className={styles.links}>
